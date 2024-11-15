4dd6fb519a619df1eeabeb865b517298a44e14f0
MOHD-ADNAN-123 <adnan10002019@gmail.com>
2024-11-15 13:35:56 +0000

    datatypes

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33"
 
-console.log(typeof score);
+//console.log(typeof score);
 //console.log(typeof (score));  CAN ALSO WRITE LIKE THIS
 
 //TO CONVERT IT FROM STRING TO NUMBER
 let value = Number(score)
-console.log(typeof value);
+//console.log(typeof value);
 
 //SIMILARLY WE HAVE String() , Boolean() etc
 
@@ -13,7 +13,7 @@ console.log(typeof value);
 //IMPORTANT
 let random = "abcd"
 let val = Number(random)
-console.log(val);     
+//console.log(val);     
 /* THE OUTPUT IS NaN (NOT A NUMBER) WHOSE TYPE IS NUMBER(a little confusing) 
 NULL --> 0
 UNDEFINED --> NaN
@@ -23,7 +23,7 @@ FOR BOOLEAN -->  True-->1 , False-->0 */
 //Boolean()
 let a = 1
 let Passs = Boolean(a)
-console.log(Passs);
+//console.log(Passs);
 
 /*
  1--> true
@@ -36,6 +36,27 @@ console.log(Passs);
 //String()
 let anynum = 10
 let StringAnynum = String(anynum)
-console.log(StringAnynum);
-console.log(typeof StringAnynum);
+//console.log(StringAnynum);
+//console.log(typeof StringAnynum);
+
+
+// ************************************** OPERATIONS *******************************************
+
+//TO GET NEGATIVE OF ANY NUMBER
+let val1 = 10
+let negVal1 = - val1   
+//console.log(negVal1);
+
+//IF THE STRING IS FIRST --> EVERYONE WILL BE CONSIDERED AS STRING
+ console.log("1"+2+2); //122
+ //FIRST EVERYTHING WILL BE ADD THEN CONVERT TO STRING
+ console.log(1+2+"2");  //32
+
+
+ //+ OPERATOR CONVERT TO NUMBER
+ console.log(+true);  
+ console.log(+"");
+ 
+ 
+
 
