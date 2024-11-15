aa1fa1870396c79f736a70151ea19249838da8e9
MOHD-ADNAN-123 <adnan10002019@gmail.com>
2024-11-15 10:54:47 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,22 @@
+"use strict"  // TREAT ALL JS CODE AS NEWER VERSION
+
+let name = "ADNAN"
+let age = 19
+let passed = true   // or false
+
+//DATATYPES USED MOSTLY
+
+//string
+//number
+//bigint
+//boolean --> true/false
+//null --> representation of empty value [not equal to zero]  
+//undefined --> when the variable is declared but the value is not assigned
+//symbols --> for uniqueness
+
+
+console.log(typeof age);
+/*
+typeof is used to get the type of any data
+*/
+console.log(typeof null);    //object type
