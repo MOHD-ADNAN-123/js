485093f34ab31a6d125fcd0c11f0efff9317b014
MOHD-ADNAN-123 <adnan10002019@gmail.com>
2025-04-26 10:30:04 +0000

    container conf done

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,16 +1,19 @@
 const accountId = 12212
-let accountEmail = "adnan10002@gmail.com"
-var accountPassword = "12345"
+let accountEmail = "adnan10002@gmail.com"  //LET AND VAR IS USED FOR VARIABLE 
+var accountPassword = "12345"              //PREFERABLY LET
 accountCity = "DELHI"    //DO NOT USE THIS METHOD , ALTHOUGH IT WORKS BUT NOT PREFERABLE AT ALL
-let accountState;
+let accountState;         // WE CAN DEFINE IT AFTERWARDS
 
 //accountId = 2 
-//WE CANNOT DO THIS AS IT WAS ASSIGNED TO A CONSTANT 
+//CONST VALUE CANNOT BE CHANGED
 
 
 /*
 prefer not to use var because of issue in block scope
 */
 
+
+//TO PRINT ONE VALUE
 console.log(accountId);
+//TO PRINT MULTIPLE VALUE
 console.table([accountId,accountEmail,accountPassword,accountState]);
