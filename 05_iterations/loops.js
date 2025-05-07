8fde0319633679afbdf9c3f856eb43873537d282
MOHD-ADNAN-123 <adnan10002019@gmail.com>
2025-05-07 16:38:41 +0000

    commit

---
+++ 05_iterations/loops.js
@@ -0,0 +1,29 @@
+//**********FOR LOOP********* */
+
+for (let i = 0; i < 10; i++) {
+    //console.log(i);   
+}
+
+//NESTED LOOPS
+for (let i = 0; i < 5; i++) {
+    for (let j = 0; j < 5; j++) {
+        //console.log(`VALUE OF I IS ${i} AND VALUE OF J IS ${j}`);
+        //console.log(i + "*" + j + "="+i*j);
+        
+    }
+    console.log(" "); 
+}
+
+//********USE OF BREAK & CONTINUE*******
+
+//BREAK
+for (let i = 0; i < 20; i++) {
+    if(i==10) break;
+    console.log(i); 
+}
+
+//CONTINUE  
+for (let i = 0; i < 20; i++) {
+    if(i==10) continue;
+    console.log(i);
+}
